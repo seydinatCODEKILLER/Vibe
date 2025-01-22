ff13321783dccfcd291495d949129138c3543765
Ben Thiam <seydinat235@gmail.com>
2025-01-22 13:37:58 +0000

    mise en place du middleware pour la gestion des fichiers statiques

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,20 @@ import cors from "cors";
 import downloadRouter from "./routes/downloadRoute.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 import connectDB from "./configuration/dbConfig.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 //Variables
 const app = express();
 const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Middleware
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //Routes de l'api
 app.use("/download", downloadRouter);
